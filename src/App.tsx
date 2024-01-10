198fab7d1b4b51dc7f01b08ef91539d292eece93
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-01-10 22:03:42 +0100

    Change function on arrow const (App)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className='App'>
+            Simple HtML
+        </div>
+    )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//      Hello, samurai! Let's go!
+//     </div>
+//   );
+// }
 
 export default App;
